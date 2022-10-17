5b797a2ecb5be7545a73d9ef9ebd1bd17c114412
soonmineom <skdlfma123@gmail.com>
2022-10-17 13:45:53 +0900

    select 해보기

--- src/main/java/com/dbexercise/UserDao.java
+++ src/main/java/com/dbexercise/UserDao.java
@@ -1,49 +1,78 @@
 package com.dbexercise;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import com.dbexercise.domain.User;
+
+import java.sql.*;
 import java.util.Map;
 
 public class UserDao {
 
-public void add() throws SQLException, ClassNotFoundException {
+    public void add() throws SQLException, ClassNotFoundException {
 
-    // 보안을 위한 환경변수 이용
-    Map< String, String> env = System.getenv();
-    String dbHost = env.get("DB_HOST");
-    String dbUser = env.get("DB_USER");
-    String dbPassword = env.get("DB_PASSWORD");
+        // 보안을 위한 환경변수 이용
+        Map<String, String> env = System.getenv();
+        String dbHost = env.get("DB_HOST");
+        String dbUser = env.get("DB_USER");
+        String dbPassword = env.get("DB_PASSWORD");
 
-    Class.forName("com.mysql.cj.jdbc.Driver");
+        Class.forName("com.mysql.cj.jdbc.Driver");
 
 //    Connection conn = DriverManager.getConnection(
 //    "jdbc:mysql://ec2-15-165-231-87.ap-northeast-2.compute.amazonaws.com/likelion", "root", "password");
-   // DB와 연결하기
-    Connection conn = DriverManager.getConnection(
-            dbHost,dbUser,dbPassword);
+        // DB와 연결하기
+        Connection conn = DriverManager.getConnection(
+                dbHost, dbUser, dbPassword);
 
-    // sql구문 작성
-    PreparedStatement ps = conn.prepareStatement(
-            "INSERT INTO users(id, name, password) VALUES(?,?,?)"
-    );
-    ps.setString(1, "2");
-    ps.setString(2,"soonmin");
-    ps.setString(3, "1234");
+        // sql구문 작성
+        PreparedStatement ps = conn.prepareStatement(
+                "INSERT INTO users(id, name, password) VALUES(?,?,?)"
+        );
+        ps.setString(1, "2");
+        ps.setString(2, "soonmin");
+        ps.setString(3, "1234");
 
-    // sql 구문 실행
-    ps.executeUpdate();
-    
-    // 다 쓴 뒤 닫기
-    ps.close();
-    conn.close();
+        // sql 구문 실행
+        ps.executeUpdate();
+
+        // 다 쓴 뒤 닫기
+        ps.close();
+        conn.close();
+    }
+
+    public User get(String id) throws ClassNotFoundException, SQLException {
+
+        Map<String, String> env = System.getenv();
+        String dbHost = env.get("DB_HOST");
+        String dbUser = env.get("DB_USER");
+        String dbPassword = env.get("DB_PASSWORD");
+
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection conn = DriverManager.getConnection(
+                dbHost, dbUser, dbPassword);
 
 
-}
+        PreparedStatement ps = conn.prepareStatement(
+                "SELECT id, name, password FROM users WHERE id = ?"
+        );
+
+        ps.setString(1, id);
+
+        ResultSet rs = ps.executeQuery();
+        rs.next();
+
+        User user = new User(rs.getString("id"),rs.getString("name"), rs.getString("password"));
+        rs.close();
+        ps.close();
+        conn.close();
+
+        return user;
+    }
+
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
-    UserDao userDao = new UserDao();
-    userDao.add();
+        UserDao userDao = new UserDao();
+//        userDao.add();
+        User user = userDao.get("1");
+        System.out.println(user.getName());
 
     }
 }
